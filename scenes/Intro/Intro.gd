0887d4a76da9f3dadcd2783234691d5ed1d4c13f
Luca Junge <LucaJunge97@gmail.com>
2021-09-25 12:25:07 +0200

    restructured scenes and scripts

---
+++ scenes/Intro/Intro.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+func _on_StartGameButton_button_up():
+	
+	# get the player name
+	var playerName = $CanvasLayer/Control/Panel/MarginContainer/CenterContainer/VBoxContainer/LineEdit.get_text()
+
+	# save in global PlayerData (TODO: rename to SavegameData)
+	PlayerData.playerName = playerName
+	
+	# create a "unique" savegame name
+	var date = OS.get_datetime()
+	var savegame_filename: String = str(playerName) + str(date.year) + str(date.month) + str(date.day) + str(date.hour) + str(date.minute) + str(date.second) + ".save"
+	
+	PlayerData.savegame_filename = savegame_filename
+	PlayerData.savegame_timestamp_created = OS.get_system_time_secs()
+	
+	# change the scene to the SceneManager
+	get_tree().change_scene("res://scenes/SceneManager/SceneManager.tscn")
---
+++ scenes/MainMenu/MainMenu.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+func _on_StartNewGameButton_button_up():
+	print("Start New Game")
+	get_tree().change_scene("res://scenes/Intro/Intro.tscn")
+
+func _on_LoadGameButton_button_up():
+	# show saved games selection screen
+	get_tree().change_scene("res://scenes/SavedGames.tscn")
+
+func _on_OptionsButton_button_up():
+	# show options menu
+	get_tree().change_scene("res://scenes/Options.tscn")
+
+func _on_Exit_button_up():
+	print("Exit")
+	get_tree().quit()
+
+func _on_Licenses_button_up():
+	$MainMenuCanvas/Licenses.visible = true
