c624e7f680b6233f2eb3cd7c013c03d9fa5b74c8
Florian <florian.huxoll@gmail.com>
2021-12-19 18:39:58 +0100

    added enter function on playerName input

--- scenes/Intro/Intro.gd
+++ scenes/Intro/Intro.gd
@@ -1,19 +1,44 @@
 extends Node2D
-
-func _on_StartGameButton_button_up():
+onready var line_edit = $CanvasLayer/Control/Panel/MarginContainer/CenterContainer/VBoxContainer/LineEdit
+func _ready():
+	line_edit.connect("text_entered", self, "start_game")
 	
+func _on_StartGameButton_button_up():
 	# get the player name
-	var playerName = $CanvasLayer/Control/Panel/MarginContainer/CenterContainer/VBoxContainer/LineEdit.get_text()
+	var playerName: String = line_edit.get_text()
+	start_game(playerName)
+
+
+func start_game(player_name: String):
+	
+	var is_valid = validate_input(player_name)
+	
+	if(not is_valid):
+		print("Not valid")
 
 	# save in global PlayerData (TODO: rename to SavegameData)
-	PlayerData.playerName = playerName
+	#PlayerData.playerName = playerName
 	
 	# create a "unique" savegame name
-	var date = OS.get_datetime()
-	var savegame_filename: String = str(playerName) + str(date.year) + str(date.month) + str(date.day) + str(date.hour) + str(date.minute) + str(date.second) + ".save"
+	#var date = OS.get_datetime()
+	#var savegame_filename: String = str(playerName) + str(date.year) + str(date.month) + str(date.day) + str(date.hour) + str(date.minute) + str(date.second) + ".save"
 	
-	PlayerData.savegame_filename = savegame_filename
-	PlayerData.savegame_timestamp_created = OS.get_system_time_secs()
+	#PlayerData.savegame_filename = savegame_filename
+	#PlayerData.savegame_timestamp_created = OS.get_system_time_secs()
 	
 	# change the scene to the SceneManager
-	get_tree().change_scene("res://scenes/SceneManager/SceneManager.tscn")
+	#get_tree().change_scene("res://scenes/SceneManager/SceneManager.tscn")
+
+func validate_input(input: String):
+	var regex = RegEx.new()
+	var expression = "[A-Za-zöäüÖÄÜß]+"
+	regex.compile(expression)
+	
+	var result = regex.search(input)
+	
+	if result:
+		if result.get_string() == input:
+			print(result.get_string() + " = " + input)
+			return true
+
+	return false
