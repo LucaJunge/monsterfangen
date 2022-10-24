f2668dd5c73dfd532523b69985d63b01d04e79c8
Luca Junge <LucaJunge97@gmail.com>
2022-10-24 20:26:13 +0200

    added theming for items menu

--- scenes/UI/Panels/ItemsMenu/ItemsMenu.gd
+++ scenes/UI/Panels/ItemsMenu/ItemsMenu.gd
@@ -1,4 +1,10 @@
 extends Panel
 
+onready var click = load("res://assets/sounds/click.wav")
+
 func _ready() -> void:
 	self.visible = false
+
+func _on_BackButton_pressed() -> void:
+	AudioManager.play(click)
+	SceneTransition.change_overlay(self, "fade")
